cb52c0d62c145774807948343980994dd304fff8
Kkabir Bhalla <kkabirbhalla@gmail.com>
2026-07-20 23:50:32 +0800

    update: layout

--- frontend/restqpifrontend/src/app/layout.js
+++ frontend/restqpifrontend/src/app/layout.js
@@ -1,25 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "ResQPi",
-  description: "Rescue people (an a raspberry pi, get it?)",
+    title: "ResQPi",
+    description: "Rescue people from emergencies!",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
